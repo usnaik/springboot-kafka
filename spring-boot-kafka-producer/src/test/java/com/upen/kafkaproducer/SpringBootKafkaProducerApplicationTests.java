8d97e8d2f986061abe09fa5d5a75d9a846fa7246
usnaik <upendra_naik@yahoo.com>
2019-11-03 15:35:43 +0530

    Initial project creation

---
+++ spring-boot-kafka-producer/src/test/java/com/upen/kafkaproducer/SpringBootKafkaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upen.kafkaproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKafkaProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
